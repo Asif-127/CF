2977355e85b7b386cea8a4ef1cdcfff7c0b19348
Mohammad Asif <asif.nwu.cse@gmail.com>
2024-10-16 21:38:26 +0600

    Summation.c
    Summation from 1 to N

---
+++ Summation.c
@@ -0,0 +1,12 @@
+//Summation from 1 to N
+#include<stdio.h>
+int main()
+{
+    long long N;
+    scanf("%lld",&N);
+    
+    long long sum = (N*(N+1))/2;
+    printf("%lld\n",sum);
+
+    return 0;
+}
