fb4d931b9a7dc0fc6fe4ef061318888b0ba9e859
Mohammad Asif <asif.nwu.cse@gmail.com>
2024-10-16 21:47:09 +0600

    Watermelon.c

--- Watermelon.c
+++ Watermelon.c
@@ -1,1 +1,18 @@
-
+//Watermelon 
+#include<stdio.h>
+int main()
+{
+    int w;
+    printf("Enter the Waight of Watermelon ");
+    scanf("%d",&w);
+    
+    if(w%2==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+  return 0;  
+}
