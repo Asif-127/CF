5c4d9fbca864dad7319a00be33f8fb13951360e1
Mohammad Asif <asif.nwu.cse@gmail.com>
2024-10-18 18:47:22 +0600

    Create Next Alphabet.c

---
+++ Next Alphabet.c
@@ -0,0 +1,21 @@
+//Next Alphabet
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    
+    if(a=='z')
+    {
+        printf("a\n");
+        
+    }
+    else 
+    {
+        printf("%c\n",a+1);
+    }
+    
+    
+    return 0;
+
+}
